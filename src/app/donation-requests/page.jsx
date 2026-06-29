46bcd8989eb80112dcd06076402a56207a5cf7cc
shakhaoyat <shakhaoyathossain.12@gmail.com>
2026-06-29 21:09:29 +0600

    navber update

---
+++ src/app/donation-requests/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+      return (
+            <div>
+                  donation-requests
+            </div>
+      );
+};
+
+export default page;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Footer from "@/components/Footer";
-
+import Navber from "@/components/Navber";
 
 export default function Home() {
   return (
-    <div>home
+    <div>
+      <Navber />
+      home
       <Footer />
     </div>
 
