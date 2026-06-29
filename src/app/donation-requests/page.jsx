394f08478dff3dabc65580ff863e6802ec5608ba
shakhaoyat <shakhaoyathossain.12@gmail.com>
2026-06-29 23:49:56 +0600

    correct func

--- src/app/donation-requests/page.jsx
+++ src/app/donation-requests/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const page = () => {
       return (
             <div>
-                  donation-requests
+                  donation requests
             </div>
       );
 };
